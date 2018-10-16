dfa03ea867adf6fa038e686ff4358ad38c2b6ba7
Ashvin Coca <AshvinCoca@gmail.com>
2018-10-16 14:06:44 -0700

    adding server functionality

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,5 @@
 //build our method for converting json to csv
 
+var storage = [];
+
+export default app;
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const client = require('./client/app.js')
 
 
 //this line tells the server to serve up index.html for any requests to '/'
 app.use(express.static('client'));
 
+app.post('/upload_json', (req, res) => {
+    
+});
 
 
 app.listen(port, () => console.log('Express is listening on port', port))
