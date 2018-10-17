66ad93e448a91f10a41c86a4ab594c0f8a8865af
Ashvin Coca <AshvinCoca@gmail.com>
2018-10-17 12:29:48 -0700

    initial commit, not working

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Component} from 'react';
+
+class App extends Component {
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+
+    //     }
+    // }
+
+    render() {
+        return (
+            <div id="form container">Hi
+            <form id="name">
+                <input type="text" placeholder="Enter name"></input>
+                <input type="text" placeholder="Enter email"></input>
+                <input type="text" placeholder="Enter password"></input>
+                <input type="submit">Submit</input>
+            </form>
+            </div>
+        )
+    }
+}
 
 
+ReactDOM.render(<App />, document.getElementById("app"))
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+var bodyParser = require('body-parser')
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
+
+app.listen(port, () => console.log('Express is listening on port', port))
