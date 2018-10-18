02e574a03ac69673ebd915f79a2a611f6d4af109
Ashvin Coca <AshvinCoca@gmail.com>
2018-10-18 16:01:31 -0700

    making battleship instead of connect 4

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,11 @@
+const express = require("express");
+const app = express();
+// const path = require('path');
+const port = 3000;
 
 
+app.use(express.static('client/dist'))
+
+
+
+app.listen(port, () => console.log('Express is listening on port', port))
