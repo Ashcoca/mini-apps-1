bf6a56fc71af4867b8666abb0f28eab7102a65c6
Ashvin Coca <AshvinCoca@gmail.com>
2018-10-17 17:21:00 -0700

    need to reboot commit

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -3,22 +3,28 @@
 // import {Component} from 'react';
 
 class App extends React.Component {
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
+    constructor(props) {
+        super(props)
+        this.state = {
+            form1: ["Name", "E-mail", "Password"],
+            form2: ["Address Line One", "Address Line Two", "City, ST"],
+            form3: ["Credit Card", "cvv", "Zip Code"]
+        }
+    }
 
-    //     }
-    // }
 
     render() {
         return (
-            <div id="form container">Testing
-            {/* <form id="name">
-                <input type="text" placeholder="Enter name"></input>
-                <input type="text" placeholder="Enter email"></input>
-                <input type="text" placeholder="Enter password"></input>
-                <br/><input type="submit">Submit</input>
-            </form> */}
+            <div id="form container">
+            <form id="name">
+                {this.state.label.form1[1]}
+                <input type="text"></input><br/>
+                {this.state.label.form1[1]}
+                <input type="text"></input><br/>
+                {this.state.label.form1[1]}
+                <input type="text"></input><br/>
+                <input type="submit"></input>
+            </form>
             </div>
         )
     }
