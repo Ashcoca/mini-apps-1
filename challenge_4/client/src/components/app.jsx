5ef94697d411370d54c4b40c648b2065c051254c
Ashvin Coca <AshvinCoca@gmail.com>
2018-10-18 17:25:53 -0700

    going back to connect 4

---
+++ challenge_4/client/src/components/app.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            board: [
+                    [ [null], [null], [null], [null], [null], [null], [null] ],
+                    [ [null], [null], [null], [null], [null], [null], [null] ],
+                    [ [null], [null], [null], [null], [null], [null], [null] ],
+                    [ [null], [null], [null], [null], [null], [null], [null] ],
+                    [ [null], [null], [null], [null], [null], [null], [null] ],
+                    [ [null], [null], [null], [null], [null], [null], [null] ]
+            ]
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+
+
+
+export default App;
